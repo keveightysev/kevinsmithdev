499c26d68459e087374fe0f4690683b661c3d8c3
Kevin Smith <keveightysev@gmail.com>
2022-05-29 14:32:15 -0700

    Upgraded packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import styled from "styled-components";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { Background, Nav, About, Contact, Work, Project } from "./components";
+import { Background, Nav, About, Contact, Work, Project } from './components';
 
 function App() {
   return (
-    <main className="App">
+    <main className='App'>
       <Container>
         <Nav />
-        <Switch>
-          <Route path="/about" component={About} />
-          <Route path="/work" component={Work} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/project/:slug" component={Project} />
-          <Route render={() => <Redirect to="/" />} />
-        </Switch>
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/work' element={<Work />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/project/:slug' element={<Project />} />
+          <Route render={() => <Navigate to='/' />} />
+        </Routes>
       </Container>
       <Background />
     </main>
@@ -25,7 +25,6 @@ export default App;
 
 const Container = styled.div`
   position: absolute;
-  // overflow: auto;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
